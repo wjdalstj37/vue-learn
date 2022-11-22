97ce0cec09f02bf02f4adbab0ee572cb37243ae0
wjdalstj37 <jms6020@gmail.com>
2022-11-22 18:07:54 +0900

    feat: learn CLI

---
+++ vue-cli/src/singleCom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>header</div>
+  <!-- HTML -->
+</template>
+
+<script>
+export default {
+  // Javascript - 인스턴스 옵션
+  methods: {
+    addNum: function () {},
+  },
+};
+</script>
+
+<style>
+/* CSS */
+</style>
